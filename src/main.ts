4e149ca06f70c55d124104067467c4ffbf27efc8
DiRaiks <t.fane@mail.ru>
2019-12-06 16:18:33 +0300

    add production command, add sert

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import * as fs from 'fs';
 import * as RateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 
+let httpsOptions = {}
+
+if (process.env.NODE_ENV === 'production') {
+  httpsOptions = {
+    key: fs.readFileSync('./privkey.pem'),
+    cert: fs.readFileSync('./fullchain.pem'),
+  };
+}
 const rateLimiter = new RateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
@@ -13,7 +22,7 @@ const registrationLimiter = new RateLimit({
 });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, httpsOptions);
   app.use(rateLimiter);
   app.use('/auth/registration', registrationLimiter);
   app.enableCors();
