ccf093c63273b3248d689306313358c546f4bfec
DiRaiks <t.fane@mail.ru>
2020-09-07 23:43:42 +0300

    change weather url

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -6,8 +6,8 @@ export class WeatherService {
   constructor(private httpService: HttpService) {}
 
   async getWeather(): Promise<Weather> {
-    const weather = await this.httpService.get('https://api.openweathermap.org/data/2.5/onecall?lat=47.222531&lon=39.718705&\n' +
-      'exclude=hourly&appid=da58e8be9d2780499a34927e283b3378&units=metric').toPromise();
+    const weather = await this.httpService.get('https://api.openweathermap.org/data/2.5/onecall' +
+      '?lat=47.222531&lon=39.718705&exclude=minutely,hourly&appid=da58e8be9d2780499a34927e283b3378&units=metric').toPromise();
 
     return weather.data;
   }
